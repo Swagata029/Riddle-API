5d46742f162592478012a3b4fa2b5cf16eb68e06
Swagata029 <swagatatalekar22@gmail.com>
2025-07-26 23:58:28 +0530

    Update

--- src/riddle.controller.ts
+++ src/riddle.controller.ts
@@ -5,7 +5,7 @@ export class RiddleController {
   private riddles = [
     {
       riddle: 'What goes up but never comes down?',
-      answer: ['age'],
+      answer: ['age', 'ages'],
     },
     {
       riddle: 'How many months of the year have 28 days?',
@@ -14,7 +14,7 @@ export class RiddleController {
     {
       riddle:
         'Pearl white chest without key or lid. Inside of which, a golden treasure is hid. What am I?',
-      answer: ['egg'],
+      answer: ['egg', 'eggs'],
     },
     {
       riddle:
@@ -24,35 +24,91 @@ export class RiddleController {
     {
       riddle:
         'I can fly but have no wings. I can cry but I have no eyes. Wherever I go, darkness follows me. What am I?',
-      answer: ['clouds'],
+      answer: ['clouds', 'cloud'],
     },
     {
       riddle: 'What has to be broken before you can use it?',
-      answer: ['egg'],
+      answer: ['egg', 'eggs'],
     },
     {
       riddle: 'People buy me to eat, but never eat me. What am I?',
-      answer: ['cutlery', 'plates', 'utensils', 'spoon', 'fork'],
+      answer: ['cutlery', 'plates', 'plate', 'utensils', 'utensil','spoon', 'spoons', 'fork', 'forks'],
     },
     {
       riddle: 'The more you take, the more you leave behind. What are they?',
-      answer: ['footsteps', 'steps'],
+      answer: ['footsteps', 'footstep', 'steps', 'step'],
     },
     {
       riddle:
         ' I’m tall when I’m young, and I’m short when I’m old. What am I?',
-      answer: ['candle'],
+      answer: ['candle', 'candles'],
     },
     {
       riddle: 'What has teeth, but cannot chew?',
-      answer: ['comb'],
+      answer: ['comb', 'combs'],
     },
     {
       riddle:
         'What’s green, but not a leaf, and mimics others, but is not a monkey?',
-      answer: ['parrot'],
+      answer: ['parrot', 'parrots'],
     },
-  ];
+    {
+      riddle: 'Light as a feather, there’s nothing in it, but the strongest man can’t hold it much more than a minute.',
+      answer: ['breath'],
+    },
+    {
+      riddle: 'I am always in front and never behind. What am I?',
+      answer: ['future'],
+    },
+    {
+      riddle: ' What runs around the whole yard without moving?',
+      answer: ['fence', 'fences'],
+    },
+    {
+      riddle: 'They have no flesh, nor feathers, nor scales, nor bone, but they do have fingers and thumbs of their own. What are they?',
+      answer: ['gloves', 'glove'],
+    },
+    {
+      riddle: ' I have a tail and a head, but no body. What am I?',
+      answer: ['coin', 'coins'],
+    },
+    {
+      riddle: ' If you have me, you will want to share me. If you share me, you will no longer have me. What am I?',
+      answer: ['secret', 'secrets'],
+    },
+    {
+      riddle: 'What comes once in a minute, twice in a moment, but never in a thousand years?',
+      answer: ['m'],
+    },
+    {
+      riddle: 'What always ends everything?',
+      answer: ['g'],
+    },
+    {
+      riddle: 'I’m the rare case when today comes before yesterday. What am I?',
+      answer: ['dictionary', 'dictionaries'],
+    },
+    {
+      riddle: 'What is full of holes but can still hold water?',
+      answer: ['sponge', 'sponges'],
+    },
+    {
+      riddle: ' What begins and ends with an ‘e’ but only contains one letter?',
+      answer: ['envelope', 'envelopes'],
+    },
+    {
+      riddle: 'After a train crashed, every single person died. Who survived?',
+      answer: ['couple', 'couples'],
+    },
+    {
+      riddle: 'A man shaves several times a day but still has a beard. Who is he?',
+      answer: ['barber'],
+    },
+    {
+      riddle: 'What is the thing that you can never eat for lunch and dinner?',
+      answer: ['breakfast'],
+    },
+  ]; //25 Riddles
 
   @Get('riddle')
   getRandomRiddles() {
