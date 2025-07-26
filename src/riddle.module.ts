66759c45cc70adc932cc225f4506045d70d0987f
Swagata029 <swagatatalekar22@gmail.com>
2025-07-26 20:55:48 +0530

    Update

--- src/riddle.module.ts
+++ src/riddle.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RiddleController } from './riddle.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [RiddleController],
 })
-export class AppModule {}
+export class RiddleModule {}
